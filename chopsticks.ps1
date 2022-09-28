3ca2e22335f1fcc1946bfe67da630aae4f86981e
Mattin <mattinmir@gmail.com>
2022-09-28 23:30:03 +0100

    evolution

--- chopsticks.ps1
+++ chopsticks.ps1
@@ -16,110 +16,220 @@ function Get-Action ($currentstate, $statemap_obj)
 }
 
 $headless = $true
-if ($headless) {$iterations = 100000} else {$iterations = 1}
+if ($headless) {$iterations = 1000} else {$iterations = 1}
+if ($headless) {$rounds = 10} else {$rounds = 1}
+
+
 $CPU = 0
 $HUMAN = 1
 
 $difficulty = 1
-$csv = ".\statemap.csv"
-$states = import-csv -Path $csv
+$states = @{}
 
-$games = @()
-foreach ($i in 0..($iterations-1))
+$stable = "v1.0"
+$experimentalMajor = "v2"
+$experimentalMinor = 10
+if (Get-ChildItem ".\statemaps\$experimentalMajor.$($experimentalMinor + 1).csv" -ErrorAction SilentlyContinue)
 {
-    $turn = (Get-Random) % 2
-    [Game] $game = [Game]::new($turn, $headless)
+    throw "Current config will overwrite existing versions"
+}
 
-    :startturn while (-not ($game.p1.dead() -or $game.p2.dead()))
+if (-not $headless)
+{
+    $states.Add(0,(import-csv -Path ".\statemaps\$stable.csv"))
+}
+else 
+{
+    $states.Add(0,(import-csv -Path ".\statemaps\v0.csv")) 
+    $states.Add(1,(import-csv -Path ".\statemaps\$experimentalMajor.$experimentalMinor.csv"))
+}
+
+
+
+foreach ($round in 1..$rounds)
+{
+    Write-Host "`nRound: $round"
+    $games = @()
+    foreach ($i in 0..($iterations-1))
     {
-        $turnplayer = $game.players[$turn]
-        if (-not $headless -and $turn -ne $CPU)
+        $turn = (Get-Random) % 2
+        [Game] $game = [Game]::new($turn, $headless)
+        :startturn while (-not ($game.p1.dead() -or $game.p2.dead()))
         {
-            Write-Host "Enter source and dest hands..." -ForegroundColor $turnplayer.colour
-            Write-Host "Left (L or [), Right (R or ]), Split (S or #)" -ForegroundColor $turnplayer.colour
-            $choice = Read-Host 
-            $src = $choice[0]
-            if ($src -eq '[') {$src = 'L'} 
-            elseif ($src -eq ']') {$src = 'R'}
-            elseif ($src -eq '#') {$src = 'S'}
-
-            if ($src -ne "S")
-            {  
-                $dest = $choice[1]  
-                if ($dest -eq '[') {$dest = 'L'} 
-                elseif ($dest -eq ']') {$dest = 'R'}
-                elseif ($dest -eq '#') {$dest = 'S'}
-            }
-        }
-        else 
-        {
-            if ($headless -and $turn -eq $cpu)
+            $turnplayer = $game.players[$turn]
+            if (-not $headless -and $turn -ne $CPU)
             {
-                $_difficulty = 0
+                Write-Host "Enter source and dest hands..." -ForegroundColor $turnplayer.colour
+                Write-Host "Left (L or [), Right (R or ]), Split (S or #)" -ForegroundColor $turnplayer.colour
+                $choice = Read-Host 
+                $src = $choice[0]
+                if ($src -eq '[') {$src = 'L'} 
+                elseif ($src -eq ']') {$src = 'R'}
+                elseif ($src -eq '#') {$src = 'S'}
+
+                if ($src -ne "S")
+                {  
+                    $dest = $choice[1]  
+                    if ($dest -eq '[') {$dest = 'L'} 
+                    elseif ($dest -eq ']') {$dest = 'R'}
+                    elseif ($dest -eq '#') {$dest = 'S'}
+                }
             }
             else 
             {
-                $_difficulty = $difficulty
-            }
-            Switch ($_difficulty)
-            {
-                0
+                Switch ($difficulty)
                 {
-                    $src = @("L","R","S")[(Get-Random) % 3]   
-                    $dest = @("L","R")[(Get-Random) % 2]   
+                    0
+                    {
+                        $src = @("L","R","S")[(Get-Random) % 3] 
+                        $dest = ""
+                        if ($src -ne "S")
+                        {
+                            $dest = @("L","R")[(Get-Random) % 2]
+                        }   
+                    }
+
+                    1
+                    {   
+                        $action = Get-Action -currentstate ($game.currentState($turn)) -statemap_obj $states[[int]($turn)]
+                        if ([String]::IsNullOrEmpty($Action))
+                        {
+                            $src = @("L","R","S")[(Get-Random) % 3]  
+                            $dest = ""
+                            if ($src -ne "S")
+                            { 
+                                $dest = @("L","R")[(Get-Random) % 2]   
+                            }
+                        }
+                        else 
+                        {
+                            $src = $action[0]
+                            $dest = $action[1]    
+                        }
+                        
+                    }
                 }
+            }
 
-                1
-                {   
-                    $action = Get-Action -currentstate ($game.currentState($turn)) -statemap_obj $states
+            if (-not $game.legalMove($turn, $src, $dest))
+            {
+                Continue :startturn
+            }
+            else 
+            {
+                $game.move($turn, $src, $dest)
+            }
+            
+            $turn = -not $turn    
+        }
 
-                    if ([String]::IsNullOrEmpty($Action))
+        if (-not $headless)
+        {
+            if ($game.p1.dead())
+            {
+                Clear-Host
+                Write-Host "`n`nWINNER!." -ForegroundColor $game.p2.colour
+            }
+            elseif ($game.p2.dead())
+            {
+                Clear-Host
+                Write-Host "`n`nWINNER!." -ForegroundColor $game.p1.colour
+            }
+            $game.history | format-table
+        }
+        $game.winner = $game.history[-1].turn
+        $games += $game
+    }
+
+    if ($headless)
+    {
+        $grouped = $games.winner | group
+        $p1wins = $grouped | where {$_.name -eq 1} | select -ExpandProperty count
+        $p2wins = $grouped | where {$_.name -eq 2} | select -ExpandProperty count
+
+        Write-Host "P1 Wins:$P1Wins`nP2 Wins:$P2Wins"
+
+        $winningMoves = @()
+        foreach ($R2 in 0..4)
+        {
+            foreach ($L2 in 0..4)
+            {
+                foreach ($R1 in 0..4)
+                {
+                    foreach ($L1 in 0..4)
                     {
-                        $src = @("L","R","S")[(Get-Random) % 3]   
-                        $dest = @("L","R")[(Get-Random) % 2]   
+                        $winningMoves += [WinningMove]::new($L1, $R1, $L2, $R2)
                     }
-                    else 
-                    {
-                        $src = $action[0]
-                        $dest = $action[1]    
-                    }
-                    
                 }
             }
         }
 
-        if (-not $game.legalMove($turn, $src, $dest))
+        Foreach ($game in $games)
         {
-            Continue :startturn
+            $history = $game.history
+            $WinnerMoves = $history | where {$_.turn -eq $game.winner}
+
+            Foreach ($move in $WinnerMoves)
+            {
+                if ($game.winner -eq 1)
+                {
+                    $winningmove = $winningMoves | where {
+                        $_.L1 -eq $move.L1 -and `
+                        $_.R1 -eq $move.R1 -and `
+                        $_.L2 -eq $move.L2 -and `
+                        $_.R2 -eq $move.R2 
+                    }
+                }
+                elseif ($game.winner -eq 2)
+                {
+                    $winningmove = $winningMoves | where {
+                        $_.L1 -eq $move.L2 -and `
+                        $_.R1 -eq $move.R2 -and `
+                        $_.L2 -eq $move.L1 -and `
+                        $_.R2 -eq $move.R1 
+                    }
+                }
+
+                $winningmove.add($move.result)
+            }
         }
-        else 
+
+        $newStates = Import-Csv .\AllStatesEmpty.csv
+        foreach ($wm in $winningMoves)
         {
-            $game.move($turn, $src, $dest)
+            $newState = $newStates | where {
+                $_.L1 -eq $wm.L1 -and `
+                $_.R1 -eq $wm.R1 -and `
+                $_.L2 -eq $wm.L2 -and `
+                $_.R2 -eq $wm.R2 
+            }
+
+            $result = $wm.GenerateWinningResult()
+
+            if (-not [String]::IsNullOrEmpty($result))
+            {
+                $newState.result = $result
+            }
         }
+
+        for ($i = 0; $i -le $newStates.length; $i++)
+        {
+            $ns = $newStates[$i]
+            if ($ns.result -eq "")
+            {
+                $oldState = $states[1] | Where {
+                    $_.L1 -eq $ns.L1 -and `
+                    $_.R1 -eq $ns.R1 -and `
+                    $_.L2 -eq $ns.L2 -and `
+                    $_.R2 -eq $ns.R2 
+                }
+                $newStates[$i].result = $oldState.result
+            }
+        }
+
         
-        $turn = -not $turn    
+        $experimentalMinor++
+        $newStates | Export-Csv -Path ".\statemaps\$experimentalMajor.$experimentalMinor.csv" -UseQuotes Never
+        $states[1] = (import-csv -Path ".\statemaps\$experimentalMajor.$experimentalMinor.csv")
     }
-
-    if (-not $headless)
-    {
-        if ($game.p1.dead())
-        {
-            Clear-Host
-            Write-Host "`n`nWINNER!." -ForegroundColor $game.p2.colour
-        }
-        elseif ($game.p2.dead())
-        {
-            Clear-Host
-            Write-Host "`n`nWINNER!." -ForegroundColor $game.p1.colour
-        }
-        $game.history | format-table
-    }
-    $game.winner = $game.history[-1].turn
-    $games += $game
 }
-
-$grouped = $games.winner | group
-$p1wins = $grouped | where {$_.name -eq 1} | select -ExpandProperty count
-$p2wins = $grouped | where {$_.name -eq 2} | select -ExpandProperty count
-
-Write-Host "P1 Wins:$P1Wins`nP2 Wins:$P2Wins"
--- move.psm1
+++ move.psm1
@@ -19,3 +19,34 @@ class Move
 
     }
 }
+
+class WinningMove
+{
+    [uint32]$L1
+    [uint32]$R1
+    [uint32]$L2
+    [uint32]$R2
+    [String[]]$results
+    [String]$winningresult
+
+    WinningMove([uint32]$_L1,[uint32]$_R1,[uint32]$_L2,[uint32]$_R2)
+    {
+        $this.L1 = $_L1
+        $this.R1 = $_R1
+        $this.L2 = $_L2
+        $this.R2 = $_R2
+    }
+
+    [Void] add([String] $result)
+    {
+        $this.results += $result
+        #$this.winningresult = $this.results | Group-Object | Sort-Object Count -descending | Select-Object -First 1 -ExpandProperty Name
+    }
+
+    [String] GenerateWinningResult()
+    {
+        $this.winningresult = $this.results | Group-Object | Sort-Object Count -descending | Select-Object -First 1 -ExpandProperty Name
+        return $this.winningresult
+    }
+
+}
